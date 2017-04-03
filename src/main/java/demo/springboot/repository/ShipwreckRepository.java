136e78cb0805803bbf9c25f22fa94105dd72e42e
Umar Zahoor <umarzahoor@live.in>
2017-04-03 19:39:05 +0530

    Fixed repository issue

--- src/main/java/demo/springboot/repository/ShipwreckRepository.java
+++ src/main/java/demo/springboot/repository/ShipwreckRepository.java
@@ -3,6 +3,7 @@ package demo.springboot.repository;
 import demo.springboot.model.Shipwreck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
+import java.util.List;
 
+public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
 }
